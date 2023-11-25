92b2db8ba7890ed70a0f19d028a2fc2775a4e3f9
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-11-25 13:29:48 +0100

    features: models(user-model + toke-model)

--- models/user-model.js
+++ models/user-model.js
@@ -1,1 +1,10 @@
 import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+  email: { type: String, unique: true, require: true },
+  password: { type: String, require: true },
+  verify: { type: Boolean, default: false },
+  verificationLink: { type: String },
+});
+
+export const User = model("User", UserSchema);
